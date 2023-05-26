339040e0c29a30c419b671a46dc340bcb05f4f45
Victor Mafra <victorvmafra97@gmail.com>
2023-05-26 13:39:33 -0300

    Added max and min flights

--- src/controllers/flights.controllers.js
+++ src/controllers/flights.controllers.js
@@ -9,7 +9,10 @@ export async function getFlightCities(req, res){
      const flights = await getFlightsRepository(id)
      if (flights.rowCount === 0) return res.status(404).send("No Flights avaiable for this destination")
 
-      const flightsObj = await db.query(`SELECT flights.id AS id, 
+      const flightsObj = await db.query(`SELECT 
+      MIN(flights.price) AS minPrice,
+      MAX(flights.price) AS maxPrice,
+      flights.id AS id, 
       price, 
       city1.name AS "departureCity", 
       city2.name AS "arrivalCity",
@@ -19,17 +22,19 @@ export async function getFlightCities(req, res){
       JOIN cities city2 ON flights."idCityTo" = city2.id
       GROUP BY flights.id, city1.name, city2.name`)
 
-    const flightsDetails = flightsObj.rows.map(item => {
-      const departureTime = dayjs(item.departureTime);
-    
-      const departureDay = departureTime.format('DD-MM-YYYY');
-      const departureHour = departureTime.format('HH:mm');
-    
+      const flightsDetails = {
+        minPrice: flightsObj.rows[0].minprice,
+        maxPrice: flightsObj.rows[0].maxprice,
+        flights: flightsObj.rows.map(item => {
+          const departureTime = dayjs(item.departureTime);
+          const departureDay = departureTime.format('DD-MM-YYYY');
+          const departureHour = departureTime.format('HH:mm');
+        
+          return { ...item, departureDay, departureHour };
+        })
+      };
+      res.send(flightsDetails).status(200)
 
-    return {...item, departureDay, departureHour}
-  })
-     res.send(flightsDetails).status(200)
-     
     } catch (err) {
      res.status(500).send(err.message)
    }
